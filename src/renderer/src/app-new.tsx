d6ba3c5389069458611075c1da70b2cd4060519f
Patryk Przybylski <darknoriss@gmail.com>
2025-11-15 14:47:35 +0100

    Refactor application structure by introducing a new Home component and a ThemeProvider for theme management. Update AppNew to utilize the new Home component and apply global CSS changes for improved styling.

--- src/renderer/src/app-new.tsx
+++ src/renderer/src/app-new.tsx
@@ -1,31 +1,12 @@
-import { Button } from "@/components/ui/button"
-
-import electronLogo from "./assets/electron.svg"
-import { VersionsNew } from "./components/versions-new"
+import { Home } from "./pages/home"
+import { ThemeProvider } from "./providers/theme-provider"
 
 export const AppNew = () => {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send("ping")
-
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-4">
-      <div className="flex size-16 items-center justify-center">
-        <img alt="logo" src={electronLogo} />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="bg-background text-foreground flex h-screen flex-col items-center justify-center gap-4">
+        <Home />
       </div>
-      <h1 className="text-2xl font-bold">Powered by electron-vite</h1>
-      <p className="text-muted-foreground text-sm">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </p>
-      <p className="text-muted-foreground text-sm">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="flex gap-2">
-        <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-          <Button>Documentation</Button>
-        </a>
-        <Button onClick={ipcHandle}>Send IPC</Button>
-      </div>
-      <VersionsNew />
-    </div>
+    </ThemeProvider>
   )
 }
