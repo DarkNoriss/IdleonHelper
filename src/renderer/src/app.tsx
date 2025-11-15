d3ee4dfdb6773af6a0e1fb187e7774182cb3e040
Patryk Przybylski <darknoriss@gmail.com>
2025-11-15 16:00:56 +0100

    Add sidebar component with collapsible navigation and UI elements, including input, separator, and tooltip components. Integrate sidebar into main application layout with responsive design support.

--- src/renderer/src/app.tsx
+++ src/renderer/src/app.tsx
@@ -1,12 +1,19 @@
+import { AppSidebar } from "./app-sidebar"
+import { SidebarInset, SidebarProvider } from "./components/ui/sidebar"
 import { Home } from "./pages/home"
 import { ThemeProvider } from "./providers/theme-provider"
 
 export const AppNew = () => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="bg-background text-foreground flex h-screen flex-col items-center justify-center gap-4">
-        <Home />
-      </div>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          {/* <div className="bg-background text-foreground flex h-screen flex-col items-center justify-center gap-4"> */}
+          <Home />
+          {/* </div> */}
+        </SidebarInset>
+      </SidebarProvider>
     </ThemeProvider>
   )
 }
