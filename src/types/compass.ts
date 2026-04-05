e6fdd3eb21f759475773eb7de2af19802608bd3c
Patryk Przybylski <darknoriss@gmail.com>
2026-04-05 07:13:35 +0200

    feat(compass): add abomination slayer node images and config entries

--- src/types/compass.ts
+++ src/types/compass.ts
@@ -65,4 +65,134 @@ export const COMPASS_NODE_DEFS: CompassNodeDef[] = [
     label: "Stardust Hoarding",
     image: "compass/fighter-path/compass_stardust_hoarding",
   },
+  {
+    id: "abomination-slayer-i",
+    label: "Abomination Slayer I",
+    image: "compass/abominations/compass_abomination_slayer_i",
+  },
+  {
+    id: "abomination-slayer-ii",
+    label: "Abomination Slayer II",
+    image: "compass/abominations/compass_abomination_slayer_ii",
+  },
+  {
+    id: "abomination-slayer-iii",
+    label: "Abomination Slayer III",
+    image: "compass/abominations/compass_abomination_slayer_iii",
+  },
+  {
+    id: "abomination-slayer-iv",
+    label: "Abomination Slayer IV",
+    image: "compass/abominations/compass_abomination_slayer_iv",
+  },
+  {
+    id: "abomination-slayer-v",
+    label: "Abomination Slayer V",
+    image: "compass/abominations/compass_abomination_slayer_v",
+  },
+  {
+    id: "abomination-slayer-vi",
+    label: "Abomination Slayer VI",
+    image: "compass/abominations/compass_abomination_slayer_vi",
+  },
+  {
+    id: "abomination-slayer-vii",
+    label: "Abomination Slayer VII",
+    image: "compass/abominations/compass_abomination_slayer_vii",
+  },
+  {
+    id: "abomination-slayer-viii",
+    label: "Abomination Slayer VIII",
+    image: "compass/abominations/compass_abomination_slayer_viii",
+  },
+  {
+    id: "abomination-slayer-ix",
+    label: "Abomination Slayer IX",
+    image: "compass/abominations/compass_abomination_slayer_ix",
+  },
+  {
+    id: "abomination-slayer-x",
+    label: "Abomination Slayer X",
+    image: "compass/abominations/compass_abomination_slayer_x",
+  },
+  {
+    id: "abomination-slayer-xi",
+    label: "Abomination Slayer XI",
+    image: "compass/abominations/compass_abomination_slayer_xi",
+  },
+  {
+    id: "abomination-slayer-xii",
+    label: "Abomination Slayer XII",
+    image: "compass/abominations/compass_abomination_slayer_xii",
+  },
+  {
+    id: "abomination-slayer-xiii",
+    label: "Abomination Slayer XIII",
+    image: "compass/abominations/compass_abomination_slayer_xiii",
+  },
+  {
+    id: "abomination-slayer-xiv",
+    label: "Abomination Slayer XIV",
+    image: "compass/abominations/compass_abomination_slayer_xiv",
+  },
+  {
+    id: "abomination-slayer-xv",
+    label: "Abomination Slayer XV",
+    image: "compass/abominations/compass_abomination_slayer_xv",
+  },
+  {
+    id: "abomination-slayer-xvi",
+    label: "Abomination Slayer XVI",
+    image: "compass/abominations/compass_abomination_slayer_xvi",
+  },
+  {
+    id: "abomination-slayer-xvii",
+    label: "Abomination Slayer XVII",
+    image: "compass/abominations/compass_abomination_slayer_xvii",
+  },
+  {
+    id: "abomination-slayer-xviii",
+    label: "Abomination Slayer XVIII",
+    image: "compass/abominations/compass_abomination_slayer_xviii",
+  },
+  {
+    id: "abomination-slayer-xix",
+    label: "Abomination Slayer XIX",
+    image: "compass/abominations/compass_abomination_slayer_xix",
+  },
+  {
+    id: "abomination-slayer-xx",
+    label: "Abomination Slayer XX",
+    image: "compass/abominations/compass_abomination_slayer_xx",
+  },
+  {
+    id: "abomination-slayer-xxi",
+    label: "Abomination Slayer XXI",
+    image: "compass/abominations/compass_abomination_slayer_xxi",
+  },
+  {
+    id: "abomination-slayer-xxii",
+    label: "Abomination Slayer XXII",
+    image: "compass/abominations/compass_abomination_slayer_xxii",
+  },
+  {
+    id: "abomination-slayer-xxiii",
+    label: "Abomination Slayer XXIII",
+    image: "compass/abominations/compass_abomination_slayer_xxiii",
+  },
+  {
+    id: "abomination-slayer-xxiv",
+    label: "Abomination Slayer XXIV",
+    image: "compass/abominations/compass_abomination_slayer_xxiv",
+  },
+  {
+    id: "abomination-slayer-xxv",
+    label: "Abomination Slayer XXV",
+    image: "compass/abominations/compass_abomination_slayer_xxv",
+  },
+  {
+    id: "abomination-slayer-xxvi",
+    label: "Abomination Slayer XXVI",
+    image: "compass/abominations/compass_abomination_slayer_xxvi",
+  },
 ];
