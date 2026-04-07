be395b61a7c4fa674cb42a8d220c408af4f73d63
Patryk Przybylski <darknoriss@gmail.com>
2026-04-07 12:11:26 +0200

    feat(card-presets): add cards module barrel export

---
+++ src/main/scripts/general/cards/index.ts
@@ -0,0 +1,8 @@
+export {
+  CARD_CATEGORIES,
+  CARD_CATEGORY_BOTTOM,
+  CARD_CATEGORY_TOP,
+  type Card,
+  type CardCategory,
+} from "./card-categories";
+export { navigateToCategory } from "./card-navigation";
