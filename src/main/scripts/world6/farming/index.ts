28e0483ce35e872a6c4125a22cf7bf8ca0945622
Patryk Przybylski <darknoriss@gmail.com>
2026-04-08 11:11:15 +0200

    feat(farming): register bean trading scripts

--- src/main/scripts/world6/farming/index.ts
+++ src/main/scripts/world6/farming/index.ts
@@ -1,2 +1,4 @@
+export { default as beanTradingDebug } from "./bean-trading-debug";
+export { default as beanTradingGetTickets } from "./bean-trading-get-tickets";
 export { default as farmingLockUnlock } from "./farming-lock-unlock";
 export { default as farmingStart } from "./farming-start";
--- src/main/scripts/world6/index.ts
+++ src/main/scripts/world6/index.ts
@@ -1,9 +1,16 @@
-import { farmingLockUnlock, farmingStart } from "./farming";
+import {
+  beanTradingDebug,
+  beanTradingGetTickets,
+  farmingLockUnlock,
+  farmingStart,
+} from "./farming";
 import { summoningStartAutobattler, summoningStartEndless } from "./summoning";
 
 export const world6Scripts = [
   farmingStart,
   farmingLockUnlock,
+  beanTradingDebug,
+  beanTradingGetTickets,
   summoningStartEndless,
   summoningStartAutobattler,
 ];
