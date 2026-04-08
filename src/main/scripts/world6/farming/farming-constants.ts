439139d12b7b070ce6c86112365f96713e54fc65
Patryk Przybylski <darknoriss@gmail.com>
2026-04-08 10:59:02 +0200

    feat(farming): add bean trading constants

--- src/main/scripts/world6/farming/farming-constants.ts
+++ src/main/scripts/world6/farming/farming-constants.ts
@@ -1,3 +1,5 @@
+import type { Point } from "../../../backend/backend-types";
+
 export const FARMING_GRID = {
   X_STEP: 86,
   Y_STEP: 111,
@@ -8,3 +10,10 @@ export const FARMING_GRID = {
     y: 121,
   },
 } as const;
+
+// Bean trading
+export const BEAN_TRADING_TICKET_COUNT = 37;
+
+// These coordinates will be populated after running the debug script
+export const STORAGE_CENTER: Point = { x: 0, y: 0 };
+export const INVENTORY_CENTER: Point = { x: 0, y: 0 };
