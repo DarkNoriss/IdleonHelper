f9c6ffee1f87f98d498e0e1fa3db6346ee89a027
Patryk Przybylski <darknoriss@gmail.com>
2026-04-08 11:20:17 +0200

    feat(farming): add bean trading buttons to farming page

--- src/renderer/src/app/pages/world-6/farming/index.tsx
+++ src/renderer/src/app/pages/world-6/farming/index.tsx
@@ -5,6 +5,14 @@ const Farming = () => (
     actions={[
       { label: "Start Farming", scriptId: "world6.farming.start" },
       { label: "Lock/Unlock Crops", scriptId: "world6.farming.lockUnlock" },
+      {
+        label: "Bean Trading - Get Tickets",
+        scriptId: "world6.farming.beanTradingGetTickets",
+      },
+      {
+        label: "Bean Trading - Debug",
+        scriptId: "world6.farming.beanTradingDebug",
+      },
     ]}
     title="Farming"
   />
