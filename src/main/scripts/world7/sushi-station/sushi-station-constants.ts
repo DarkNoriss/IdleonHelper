57029ebfdec317d62ff39313afa214aebb884dc7
Patryk Przybylski <darknoriss@gmail.com>
2026-04-16 00:12:29 +0200

    feat(sushi-station): add tiers 23 and 24

--- src/main/scripts/world7/sushi-station/sushi-station-constants.ts
+++ src/main/scripts/world7/sushi-station/sushi-station-constants.ts
@@ -24,6 +24,7 @@ export const SUSHI_COOK = `${SUSHI_PATH}/sushi_cook`;
 
 const SUSHI_TIERS = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24,
 ];
 
 export const SUSHI_TEMPLATES = SUSHI_TIERS.map(
