c0a92c17dcd11aba43f1eb4b0afcc13fedd228e2
Patryk Przybylski <darknoriss@gmail.com>
2026-04-26 20:53:14 +0200

    feat(sushi-station): register max-buff merge in world7 registry

--- src/main/scripts/world7/sushi-station/index.ts
+++ src/main/scripts/world7/sushi-station/index.ts
@@ -1,2 +1,3 @@
+export { default as sushiStationMaxBuffMerge } from "./sushi-station-max-buff-merge";
 export { default as sushiStationMerge } from "./sushi-station-merge";
 export { default as sushiStationMergeDebug } from "./sushi-station-merge-debug";
