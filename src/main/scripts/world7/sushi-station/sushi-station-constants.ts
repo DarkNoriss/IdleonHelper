17449f0973c370dc1357219a286d928ad515b49c
DarkNoriss <104274385+DarkNoriss@users.noreply.github.com>
2026-04-27 02:33:13 +0200

    feat(sushi-station): add tiers 29 and 30 (#73)

--- src/main/scripts/world7/sushi-station/sushi-station-constants.ts
+++ src/main/scripts/world7/sushi-station/sushi-station-constants.ts
@@ -27,7 +27,7 @@ export const GRID_SLOT_YELLOW = `${SUSHI_PATH}/grid_slot_yellow`;
 
 const SUSHI_TIERS = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 28,
+  23, 24, 25, 26, 27, 28, 29, 30,
 ];
 
 export const SUSHI_TEMPLATES = SUSHI_TIERS.map(
