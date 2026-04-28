a7d53ef350ee6445ffb8ea51f747a665be57fcd5
Patryk Przybylski <darknoriss@gmail.com>
2026-04-28 17:34:48 +0200

    feat(sushi-station): add tier 46-55 templates

--- src/main/scripts/world7/sushi-station/sushi-station-constants.ts
+++ src/main/scripts/world7/sushi-station/sushi-station-constants.ts
@@ -36,7 +36,7 @@ export const GRID_SLOT_YELLOW = `${SUSHI_PATH}/grid_slot_yellow`;
 const SUSHI_TIERS = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
   23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
-  42, 43, 44, 45,
+  42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55,
 ];
 
 export const MAX_TEMPLATE_TIER = SUSHI_TIERS.at(-1)!;
