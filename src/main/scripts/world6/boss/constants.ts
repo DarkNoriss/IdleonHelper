812756b17176734c19499475a11dedde18858470
Patryk Przybylski <darknoriss@gmail.com>
2026-05-08 16:53:37 +0200

    fix(w6-boss): wait 5s after entering boss arena

--- src/main/scripts/world6/boss/constants.ts
+++ src/main/scripts/world6/boss/constants.ts
@@ -9,7 +9,7 @@ export const ENTER_POINT = { x: 220, y: 480 } as const;
 export const EXIT_BUTTON_POINT = { x: 25, y: 480 } as const;
 export const PORTAL_POINT = { x: 30, y: 420 } as const;
 
-export const POST_ENTER_DELAY_MS = 1000;
+export const POST_ENTER_DELAY_MS = 5000;
 export const POST_LINE_DELAY_MS = 5000;
 export const POST_EXIT_DELAY_MS = 5000;
 
